6e0b6a298db73910486e0eb0708792c1b697eea9
StephyStuff <stephanie.birch13@gmail.com>
2016-11-30 14:51:12 -0500

    Moved data from controller to service. Table is still viewable and able to add new item.

--- js/item.controller.js
+++ js/item.controller.js
@@ -4,27 +4,14 @@
   angular.module('shopular')
   .controller('ItemController', ItemController);
 
-  ItemController.$inject = [ '$location', 'StorageService' ];
+  ItemController.$inject = [ 'StorageService' ];
 
-  function ItemController (){
+  function ItemController (storageService){
     this.newItem = {};
+    this.allData = storageService.getAll();
+
     this.tax=1.575;
     this.orderBy = 'name';
-
-    this.items=[
-      { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
-      { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
-      { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
-      { "id": 87363, "name": "bonzai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
-      { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
-      { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
-      { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
-      { "id": 46, "name": "guitar", "price": 899, "quantity": 0, "color": "blue", "discount": 150 },
-      { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
-      { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
-      { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
-      { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
-    ];
 
     this.uk = false;
     this.currencyFormat = '$';
@@ -59,12 +46,12 @@
 
     this.addNewItem = function addNewItem(item){
       console.log('we are in here!', item);
-      this.items.push({
-        name: this.newItem.name,
-        price: this.newItem.price,
-        quantity: this.newItem.quantity,
-        color: this.newItem.color,
-        discount: this.newItem.discount
+      this.allData.push({
+        name: item.name,
+        price: item.price,
+        quantity: item.quantity,
+        color: item.color,
+        discount: item.discount
       });
     };
   }
