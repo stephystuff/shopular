902df38e074aad2319c542a1f35bc871de34fe6f
StephyStuff <stephanie.birch13@gmail.com>
2016-12-02 15:51:00 -0500

    Service is able to look inside of array and get the correct color of an index.

--- test/specs/storage.service.spec.js
+++ test/specs/storage.service.spec.js
@@ -16,11 +16,15 @@
       var result= StorageService.getAll();
       expect(result).to.be.an('array');
       expect(result.length).to.equal(12);
+      console.log(result[0]);
+      expect(result[0].name).to.equal('widget');
+      expect(result[5].color).to.equal('beige');
+
     });
 
-    it('should be able to save a new item when added to the array', function() {
-
-    });
+    // it('should be able to save a new item when added to the array', function() {
+    //   var result = StorageService.saveNewItem();
+    // });
 
 
 
