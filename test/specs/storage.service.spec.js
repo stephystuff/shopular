d02eed636dd3f6de868ced22340a48ac036c024b
StephyStuff <stephanie.birch13@gmail.com>
2016-12-02 14:11:03 -0500

    created directory.

---
+++ test/specs/storage.service.spec.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  var expect = chai.expect;
+
+  describe('storage service', function() {
+
+    it('should expect one plus one to equal one', function() {
+      expect(1).to.equal(1);
+    });
+
+
+  });
+
+}());
