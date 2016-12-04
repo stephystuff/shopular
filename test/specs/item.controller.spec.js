69e767ab979d63ad22e8d1e9854c11123a3e9558
StephyStuff <stephanie.birch13@gmail.com>
2016-12-04 15:29:16 -0500

    Added code for for set up of mock service and controller.

--- test/specs/item.controller.spec.js
+++ test/specs/item.controller.spec.js
@@ -1,4 +1,24 @@
 (function() {
   'use strict';
 
+  var expect= chai.expect;
+
+  describe('item controller', function() {
+    var ItemController;
+    var mockStorageService = {};
+
+    beforeEach(module('shopular'));
+
+    beforeEach(module(function($provide){
+      $provide.value('StorageService', mockStorageService);
+    }));
+
+    beforeEach(inject(function($controller){
+      mockStorageService.getAll = function() {
+        return [id
+        ];
+      };
+    }));
+  });
+
 }());
