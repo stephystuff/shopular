7e77fd475d6b4db9c1b04d11ec1433d1fdcf7728
StephyStuff <stephanie.birch13@gmail.com>
2016-12-04 17:18:07 -0500

    Controller is able to get total price of an item with discount and tax included. Also able to sort data by category name.

--- test/specs/item.controller.spec.js
+++ test/specs/item.controller.spec.js
@@ -15,10 +15,49 @@
 
     beforeEach(inject(function($controller){
       mockStorageService.getAll = function() {
-        return [id
+        return [
+          {id: 2957, name: 'widget', price: 32, quantity: 203, color: 'red', discount: 31}
         ];
       };
+      mockStorageService.add = function(argOne) {
+        mockStorageService.add.numTimesCalled++;
+        mockStorageService.add.LastArgument = argOne;
+      };
+      mockStorageService.add.numTimesCalled = 0;
+      ItemController = $controller('ItemController');
     }));
+
+    it('should have correct scope variables', function(){
+      expect(ItemController.newItem).to.be.an('object');
+      expect(Object.keys(ItemController.newItem).length).to.equal(0);
+      expect(ItemController.allData).to.be.an('array');
+      expect(ItemController.allData.length).to.equal(1);
+    });
+
+    // it('should use the service to add a new item', function(){
+    //   ItemController.newItem.foo= 'bar';
+    //   var theItem = {};
+    //   ItemController.addNewItem(theItem);
+    //   // expect(ItemController.newItem.foo).to.be.undefined;
+    //   expect(mockStorageService.add.numTimesCalled).to.equal(1);
+    // });
+
+    it('should calculate the total price of an item with tax and discount included', function(){
+      var item = ItemController.getPrice({
+        name: 'towel',
+        price: 55,
+        discount: 10,
+        quantity: 30
+      });
+      expect(item).to.be.a('number');
+    });
+
+    it('shold sort the order of the data by category', function() {
+      ItemController.changeOrder('name');
+      expect(ItemController.orderBy).to.equal('name');
+      ItemController.changeOrder('price');
+      expect(ItemController.orderBy).to.equal('price');
+    });
   });
 
 }());
